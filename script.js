9bef457333b01c60df7fb91ec0e971109b6d7dc9
Gena3272 <genabirchenko@icloud.com>
2021-07-12 10:13:47 +0300

    task_2_correction

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// task 1 (#1)
+
+const firstNumber = prompt('firstNumber');
+const secondNumber = prompt('secondNumber');
+if(!isNaN(firstNumber) && !isNaN(secondNumber)) {
+    console.log(Number(firstNumber).toString(Number(secondNumber)));
+}
+ else {
+    console.log('Некорректный ввод!');
+}
+
+// /task 1 (#1)
+
+// task 1 (#2)
+
+const thirdNumber = parseInt(prompt('thirdNumber'));
+const fourthNumber = isNaN(thirdNumber) || parseInt(prompt('fourthNumber'));
+
+if(!isNaN(thirdNumber) && !isNaN(fourthNumber)) {
+    console.log(`Ответ: ${thirdNumber + fourthNumber}, ${thirdNumber / fourthNumber}`);
+}
+else {
+    console.log('Некорректный ввод!');
+}
+
+// /task 1 (#2)
+
 // task 2 (#1)
 
 const isEqual = (firstObj, secondObj) => {
