63232f02c2490073f93f1bef151a736efbca4dbd
Gena3272 <genabirchenko@icloud.com>
2021-07-13 11:33:33 +0300

    homework_2
    * Implemented the solution of task1

---
+++ homework2/task1.js
@@ -0,0 +1,14 @@
+/**
+ * Написать функцию, функция должна поверхностно сравнять 2 обьекта.
+ * Результат: True если обьекты идентичны, false если обьекты разные
+ * ({ a: 1, b: 1 }, { a: 1, b: 1 }) => true
+ */
+
+const isEqual = (firstObj, secondObj) => Object.keys(firstObj).every(key => firstObj[key] === secondObj[key]);
+
+const data1 = { a: 1, b: 1 };
+const data2 = { a: 1, b: 1 };
+const data3 = { a: 1, b: 1, c: 2 };
+
+console.log(isEqual(data1, data2));
+console.log(isEqual(data1, data3));
