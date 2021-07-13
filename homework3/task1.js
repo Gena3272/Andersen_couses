e863965fc92abdb5067efcd9436a892c39ecfee1
Gena3272 <genabirchenko@icloud.com>
2021-07-13 14:51:01 +0300

    homework_3
    * Implemented the solution of task1

---
+++ homework3/task1.js
@@ -0,0 +1,15 @@
+/**
+ * Дана строка: нужно написать функцию, которая будет возвращать true, если стока является палиндром, или false если нет.
+ * При этом нужно учитывать знаки препинания и пробелы.
+ */
+
+const getStringLetters = (str) => str.replace(/[^a-z]/g, '').toLowerCase();
+
+const checkPalindrome = (str) => {
+    const stringLetters = getStringLetters(str);
+    const reversedStringLetters = stringLetters.split('').reverse().join('');
+
+    return stringLetters === reversedStringLetters;
+}
+
+console.log(checkPalindrome('!an!<nan'));
